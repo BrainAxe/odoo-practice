fa566576de57f42a5800f98614d59fecc20898fa
Tanzim Rizwan <tanzimrizwan@outlook.com>
2020-03-28 00:59:41 +0600

    update pdf report generation process

--- openacademy/wizard/course_report.py
+++ openacademy/wizard/course_report.py
@@ -10,12 +10,11 @@ class CourseWizard(models.TransientModel):
 
     @api.multi
     def check_report(self):
-        data = {}
-        data['form'] = self.read(['date_from','date_to'])[0]
-        # print(data)
+        data = {
+            'model': 'openacademy.course',
+            'form': self.read(['date_from','date_to'])[0]
+        }
         return self._print_report(data)
 
     def _print_report(self, data):
-        data['form'].update(self.read(['date_from','date_to'])[0])
-        print(data)
         return self.env.ref('openacademy.action_report_course').report_action(self, data=data)
