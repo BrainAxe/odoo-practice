7249753d9e694cac2ef7c61dae818eaf9bef416e
Tanzim Rizwan <tanzimrizwan@outlook.com>
2020-03-28 01:02:22 +0600

    updated pdf report generation process

--- openacademy/reports/courses_report.py
+++ openacademy/reports/courses_report.py
@@ -6,12 +6,18 @@ class ReportCourses(models.AbstractModel):
 
     @api.model
     def get_report_values(self, docids, data=None):
-        self.model = self.env.context.get('active_model')
-        docs = self.env[self.model].browse(self.env.context.get('active_id'))
-        from_date = docs.date_from
-        to_date = docs.date_to
-        query = 'SELECT * FROM openacademy_course WHERE create_date BETWEEN \'{}\' AND \'{}\';'.format(from_date, to_date)
-        # print(query)
-        self._cr.execute(query)
-        courses = self._cr.fetchall()
+        # self.model = self.env.context.get('active_model')
+        # docs = self.env[self.model].browse(self.env.context.get('active_id'))
+        # from_date = docs.date_from
+        # to_date = docs.date_to
+        # query = 'SELECT name, description, create_date FROM openacademy_course WHERE create_date BETWEEN \'{}\' AND \'{}\';'.format(from_date, to_date)
+        # # print(query)
+        # self._cr.execute(query)
+        # courses = self._cr.fetchall()
+        from_date = data['form']['date_from']
+        to_date = data['form']['date_to']
+        if data['form']:
+            courses = self.env['openacademy.course'].search([('create_date', '>=', from_date), ('create_date', '<=', to_date)])
+        else:
+            courses = self.env['openacademy.course'].search([])
         return {'courses': courses}
