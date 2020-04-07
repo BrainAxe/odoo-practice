1951743cb5603a7f7f8e8b332b03a60f087509ad
Tanzim Rizwan <tanzimrizwan@outlook.com>
2020-04-07 19:46:25 +0600

    added xml files in manifest

--- academy/__manifest__.py
+++ academy/__manifest__.py
@@ -27,6 +27,7 @@
         'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'views/dashboard.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
